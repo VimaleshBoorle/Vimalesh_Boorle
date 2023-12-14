5f20b4f8a07768cfe7ff09e6e3bbfd9d107ca520
VimaleshBoorle <vimalesh10d@gmail.com>
2023-12-13 22:14:39 -0800

    Detail Page Commit

--- Vimalesh_Boorle/contacts/urls.py
+++ Vimalesh_Boorle/contacts/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 
-from .views import TaskCreateView, TaskListView, WelcomeView
+from .views import TaskCreateView, TaskDeleteView, TaskDetailView, TaskListView, TaskUpdateView,WelcomeView
 
 urlpatterns = [
     path("", WelcomeView.as_view(), name="welcome"),  # Add this line for the welcome page
+    path("contacts/<int:pk>/", TaskDetailView.as_view(), name="task_detail"),
     path("create/", TaskCreateView.as_view(), name="task_create"),
     path("home/", TaskListView.as_view(), name="task_list"),
 ]
